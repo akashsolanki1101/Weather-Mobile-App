2340a3e38d00e27e9ea0c12b6507976f0db1a761
akashsolanki1101 <akashsolanki2509@gmail.com>
2020-06-11 09:42:36 +0530

    Final Commit.

--- ENV.js
+++ ENV.js
@@ -1,4 +1,4 @@
 export default Keys = {
-    darkSkyKey : 'fd9ad50289d815f52899cd349d581877',
-    mapBoxKey :'pk.eyJ1IjoiYWthc2hzb2xhbmtpMTEwMSIsImEiOiJja2F4bm1ydjQwNjI1MndwNmZ0aWx2enhsIn0.HFQWRa0dgkpFGU63gFcEAQ'
+    darkSkyKey : "YOUR_API_KEY",
+    mapBoxKey :'YOUR_API_KEY'
 }
